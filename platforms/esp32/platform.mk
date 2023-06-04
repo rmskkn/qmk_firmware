be31139644bab4903e0a75ad60bf63c20cc7c1df
Roman Skakun <rm.skakun@gmail.com>
2023-06-04 20:59:47 +0100

    [jorne-esp32]: Duplicate a keyboard mappings from the jorne
    [Changes]
    1. Changed keymap.c according to jorne:default
    2. Added toolchain option for rv32 s3 chip
    
    Signed-off-by: Roman Skakun <rm.skakun@gmail.com>

--- platforms/esp32/platform.mk
+++ platforms/esp32/platform.mk
@@ -1,5 +1,4 @@
 # Compiler settings
-#
 
 TOOLCHAIN := /home/roman/.espressif/tools/xtensa-esp32s3-elf/esp-12.2.0_20230208/xtensa-esp32s3-elf/bin/xtensa-esp32s3-elf-
 CC = $(CC_PREFIX) $(TOOLCHAIN)gcc
@@ -9,3 +8,5 @@ SIZE = $(TOOLCHAIN)size
 AR = $(TOOLCHAIN)ar
 NM = $(TOOLCHAIN)nm
 
+CFLAGS := -mlongcalls
+CXXFLAGS := -mlongcalls
